9ef07c94fe0ba281c3c497c110cc5d8c5f4d9152
Randy Nguyen <Randywins1993@gmail.com>
2021-02-27 16:25:14 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Randy  Nguyen on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
